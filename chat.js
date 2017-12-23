4efee806dd368b83881898417e8180cf831b564f
SirAricost <coffee-media@users.noreply.github.com>
2017-12-23 11:04:08 -0500

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,4 @@
+$(".comment").addClass('well');
+$(".hcb-comment-tb").removeClass('hcb-comment-tb');
+$('.inputfile').removeClass('inputfile');
+$( "label[for='hcb_file']" ).text( "Upload Image" ).addClass('btn').addClass('btn-success');
