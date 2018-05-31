c51dfd9de21b6d204ebb65dd6f13122fc86b4dd5
SirAricost <coffee-media@users.noreply.github.com>
2018-05-31 16:36:14 -0400

    Update antihack.js

--- antihack.js
+++ antihack.js
@@ -4,7 +4,7 @@ Detect if DevTools is open
 https://github.com/sindresorhus/devtools-detect
 by Sindre Sorhus
 MIT License
------------------------------------------------*/
+-----------------------------------------------
 
 (function(){'use strict';var devtools={open:false,orientation:null};var threshold=160;var emitEvent=function(state,orientation){window.dispatchEvent(new CustomEvent('devtoolschange',{detail:{open:state,orientation:orientation}}));};setInterval(function(){var widthThreshold=window.outerWidth-window.innerWidth>threshold;var heightThreshold=window.outerHeight-window.innerHeight>threshold;var orientation=widthThreshold?'vertical':'horizontal';if(!(heightThreshold&&widthThreshold)&&((window.Firebug&&window.Firebug.chrome&&window.Firebug.chrome.isInitialized)||widthThreshold||heightThreshold)){if(!devtools.open||devtools.orientation!==orientation){emitEvent(true,orientation);}
 devtools.open=true;devtools.orientation=orientation;}else{if(devtools.open){emitEvent(false,null);}
@@ -15,4 +15,4 @@ if (window.devtools.open === true && document.getElementById("hcb_form_name").va
   window.location.href = "about:blank";
   }
 }
-setInterval(ModCanUseConsole, 0);
+setInterval(ModCanUseConsole, 0); */
